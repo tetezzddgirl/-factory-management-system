f0eabb621ba745698cb8294b6432ff1199d1f053
methaweeponglopisit <methaweeponglopisit@gmail.com>
2026-09-03 09:53:38 +0700

    add slip component

--- frontend/src/routes/_authenticated/wip.tsx
+++ frontend/src/routes/_authenticated/wip.tsx
@@ -14,7 +14,6 @@ import {
 } from "@/lib/api-client";
 import { toast } from "sonner";
 import { RequisitionDialog } from "@/components/requisition-dialog";
-{/* <RequisitionDialog defaultOrderID={order.orderID} onCreated={() => refetchMyOwnData()} /> */}
 
 export const Route = createFileRoute("/_authenticated/wip")({
   head: () => ({
